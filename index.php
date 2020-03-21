53880ddcb948c3f04848ef6a25258504db6d9a73
lomborg <d.lomborg@hotmail.com>
2020-03-21 18:15:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,23 @@
 <html lang="da">
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <style>
+        body{
+            background-color: paleturquoise;
+        }
+        .fa-heart{
+            color: darkred;
+        }
+    </style>
+
+
 </head>
 <body>
     <div class="container">
         <div class="row text-center mt-4 d-flex flex-column">
-            <h1>Hi Alice</h1>
+            <h1><i class="fas fa-heart"></i> Hi Alice <i class="fas fa-heart"></i></h1>
             <br>
             <h4><small>aka</small> Puma <small>aka</small> Sunshine</h4>
             <br>
