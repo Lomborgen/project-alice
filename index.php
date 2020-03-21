b2428b2521c4b46dccb627c936774bc720e5f5f1
lomborg <d.lomborg@hotmail.com>
2020-03-21 17:45:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,13 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container text-center">
-        <h1>Hi Alice</h1>
-        <h2>You have beautiful eyes</h2>
+    <div class="container">
+        <div class="row text-center mt-4">
+            <h1>Hi Alice</h1>
+            <h2><small>aka</small> Puma <small>aka</small> Sunshine</h2>
+            <h3>Todays message:</h3>
+            <p>You have beautiful eyes and you must invent a cooked meal for your lord Dennis</p>
+        </div>
     </div>
 
 </body>
